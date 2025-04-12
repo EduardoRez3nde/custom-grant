b5db8872df3ca3adf07334c9dd7b33af65a0b319
Eduardo Rezende <dudurezende426@gmail.com>
2025-04-12 18:33:25 -0400

    RegisterClient

--- src/main/java/com/customgrant/custom_grant/configuration/AuthorizationServer.java
+++ src/main/java/com/customgrant/custom_grant/configuration/AuthorizationServer.java
@@ -1,20 +1,46 @@
 package com.customgrant.custom_grant.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.*;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationEndpointConfigurer;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
+import java.util.UUID;
+
 @Configuration
 @EnableWebSecurity
+@Import(OAuth2AuthorizationServerConfiguration.class)
 public class AuthorizationServer {
+
+    @Value("${security.client.client-id}")
+    private String clientId;
+
+    @Value("${security.client.client-secret}")
+    private String clientSecret;
+
+    @Value("${security.client.redirect-uri}")
+    private String redirectUri;
 
     @Bean
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -25,7 +51,10 @@ public class AuthorizationServer {
         httpSecurity
                 .securityMatcher(auth2AuthorizationServerConfigurer.getEndpointsMatcher())
                 .with(auth2AuthorizationServerConfigurer, (authorizationServer) ->
-                        authorizationServer.oidc(Customizer.withDefaults()))
+                        authorizationServer
+                                .oidc(Customizer.withDefaults())
+                                .registeredClientRepository(registerClient())
+                )
                 .authorizeHttpRequests((authorize) ->
                         authorize.anyRequest().authenticated()
                 )
@@ -52,5 +81,38 @@ public class AuthorizationServer {
         return httpSecurity.build();
     }
 
+    @Bean
+    public RegisteredClientRepository registerClient() {
+        RegisteredClient registeredClient = RegisteredClient
+                .withId(UUID.randomUUID().toString())
+                .clientId(clientId)
+                .clientSecret(passwordEncoder().encode(clientSecret))
+                .scope("write")
+                .scope("read")
+                .redirectUri(redirectUri)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .clientSettings(ClientSettings.builder()
+                        .requireAuthorizationConsent(true)
+                        .build())
+                .build();
 
+        return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+    @Bean
+    public OAuth2AuthorizationService oAuth2AuthorizationService() {
+        return new InMemoryOAuth2AuthorizationService();
+    }
+
+    @Bean
+    public OAuth2AuthorizationConsentService oAuth2AuthorizationConsent() {
+        return new InMemoryOAuth2AuthorizationConsentService();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/customgrant/custom_grant/configuration/SecurityConfigTest.java
+++ src/main/java/com/customgrant/custom_grant/configuration/SecurityConfigTest.java
@@ -7,11 +7,22 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @Profile("test")
 public class SecurityConfigTest {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfigTest(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Bean
     public SecurityFilterChain h2SecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -22,5 +33,15 @@ public class SecurityConfigTest {
                 .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.builder()
+                .username("default")
+                .password(passwordEncoder.encode("123"))
+                .roles("ROLE_ADMIN", "ROLE_USER")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
